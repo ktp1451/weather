0c9b8d2f0b0052be9e25b44ca3536989177ad0cd
ktp1451 <76793752+ktp1451@users.noreply.github.com>
2021-03-18 16:00:49 -0500

    part 1 of 3

--- develop/script.js
+++ develop/script.js
@@ -5,12 +5,62 @@ $(document).ready(function() {
     //GIVEN a weather dashboard with form inputs
     //THEN I am presented with current and future conditions for that city and that city is added to the search history
     var searchHistoryContainer = $('#past-searches');
-    var searchForm = $('search-form');
+    var searchForm = $('#search-form');
+    var currentWeatherContainer = $('current-weather');
+    var apiKey = '37d0b771a00505f0c256d41794d1092d'; //dont' submit to github with apikey. user must submit their own
+    var baseUrl = 'https://api.openweathermap.org/data/2.5/weather?';
     //WHEN I search for a city
     searchForm.submit(function( event ) {
-        alert( "Handler for .submit() called." );
+        //alert( "Handler for .submit() called." );
         event.preventDefault();
         console.log(event);
+        // $(this) = THIS FORM THAT JUST SUBMITTED
+        var formValues = $(this).serializeArray();
+        var city = formValues[0].value;
+        //HOW TO CREATE ELELMENT WITH JQUREY SELECTOR
+        var searchTermDiv = $('<div class="past-search-term">');
+        searchTermDiv.text(city);
+        searchHistoryContainer.append(searchTermDiv);
+        console.log(formValues, city);
+        //REAL VALUE GOTTEN FROM FORM
+        searchForCityWeather(city);
+
         });
+        function searchForCityWeather(city) {
+            //&appid={API key}
+            var fullUrl = baseUrl + "q=" + city + "&appid=" +apiKey;
+            console.log(fullUrl);
+            //WHEN I view current weather conditions for that city
+            //THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+            fetch(fullUrl).then(function (response){
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                var cityName = data.name;
+                var temp = data.main.temp;
+                var humidity = data.main.humidity;
+                var weather = data.weather;
+                var wind =data.wind;
+                console.log(temp, humidity, weather, wind);
+                var cityNameDiv = $('<div class="city-name">');
+                var temoDiv = $('<div class="temp-name">');
+                var humidityDiv = $('<div class="humidity-name">');
+                var weatherDiv = $('<div class="icon-name">');
+                var windDiv = $('<div class="wind-name">');
+                cityNameDiv.text(cityName);
+                tempDiv.text("Temperature: " + temp);
+                humidityDiv.text("Humidity: "+ humidity + "%"); 
+                windDiv.text("Wind Speed: " + wind.speed + "MPH");
+
+                currentWeatherContainer.append(cityNameDiv);
+                currentWeatherContainer.append(tempDiv);
+                currentWeatherContainer.append(humidityDiv);
+                currentWeatherContainer.append(windDiv);
+            });
+        }
+        function searchForFiveDayForecastWeather (city) {
+            
+        }
 });
 
